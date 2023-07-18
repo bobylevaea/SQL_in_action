6587d18562e5c4f02f644d243a931a98672f59cd
Ekaterina Bobyleva <129331488+bobylevaea@users.noreply.github.com>
2023-07-18 13:56:54 +0300

    Update 08_03.sql

--- 08_JOIN/08_03.sql
+++ 08_JOIN/08_03.sql
@@ -1,6 +1,10 @@
-SELECT a.user_id AS user_id_left, u.user_id AS user_id_right,  order_id, time, action, sex, u.birth_date
-FROM user_actions AS a
-LEFT JOIN
-users AS u
-USING(user_id)
-ORDER BY user_id_left
+SELECT a.user_id AS user_id_left,
+       b.user_id AS user_id_right,
+       order_id,
+       time,
+       action,
+       sex,
+       birth_date
+FROM user_actions a
+LEFT JOIN users b USING (user_id)
+ORDER BY user_id_left;
