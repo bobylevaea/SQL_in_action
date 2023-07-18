9913a891763c500326d5bd6af4bfffc20256cfb2
Ekaterina Bobyleva <129331488+bobylevaea@users.noreply.github.com>
2023-07-18 13:55:10 +0300

    Update 08_02.sql

--- 08_JOIN/08_02.sql
+++ 08_JOIN/08_02.sql
@@ -1,5 +1,3 @@
-SELECT COUNT (DISTINCT a.user_id) AS users_count
-FROM user_actions AS a
-INNER JOIN
-users AS u
-USING(user_id)
+SELECT COUNT(DISTINCT a.user_id) AS users_count
+FROM user_actions a
+JOIN users b USING (user_id);
