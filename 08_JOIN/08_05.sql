e645f30c61ecc7d57765e3495a1700f80f7e83ca
Ekaterina Bobyleva <129331488+bobylevaea@users.noreply.github.com>
2023-07-18 13:22:55 +0300

    Create 08_05.sql

---
+++ 08_JOIN/08_05.sql
@@ -0,0 +1,7 @@
+SELECT a.user_id AS user_id_left, u.user_id AS user_id_right, order_id, time, action, sex, u.birth_date
+FROM user_actions AS a
+LEFT JOIN
+users AS u
+USING(user_id)
+WHERE u.user_id IS NOT NULL
+ORDER BY user_id_left
